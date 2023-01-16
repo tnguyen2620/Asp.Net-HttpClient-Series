9b7c6d200194405b5db241e75a341386651f16ef
tnguyen2620 <jamesdoam@gmail.com>
2023-01-16 13:11:30 +1100

    1.2 Use HttpRequestMessage object to send Http Requests

--- starter/CompanyEmployees.Client/CompanyEmployees.Client/Services/HttpClientCrudService.cs
+++ starter/CompanyEmployees.Client/CompanyEmployees.Client/Services/HttpClientCrudService.cs
@@ -24,40 +24,37 @@ namespace CompanyEmployees.Client.Services
 			_httpClient.Timeout = new TimeSpan(0, 0, 30);
 			//set up the accepted media type by the httpClient
 			_httpClient.DefaultRequestHeaders.Clear();
-			_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json",0.9));
-			_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
 			_options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true }; //setup case insensitive deseralization option for JsonSerializer
 		}
 
 		public async Task GetCompanies()
 		{
-			var response = await _httpClient.GetAsync("companies");
-			response.EnsureSuccessStatusCode();
-            var content = await response.Content.ReadAsStringAsync(); //read the content of the response as a string
-            var companies = new List<CompanyDto>();
-
-			if(response.Content.Headers.ContentType.MediaType == "application/json")
-			{
-                companies = JsonSerializer.Deserialize<List<CompanyDto>>(content, _options);
-            }
-			else if (response.Content.Headers.ContentType.MediaType == "text/xml")
-			{
-				var doc = XDocument.Parse(content); //parse the content into the XDocument type
-				foreach (var element in doc.Descendants())
-				{//remove the declaration
-					element.Attributes().Where(a => a.IsNamespaceDeclaration).Remove();
-					//use a localname for the name property
-					element.Name = element.Name.LocalName;
-				}
-				//create a new xmlserializer and deserialize our XDocument
-				var serializer = new XmlSerializer(typeof(List<CompanyDto>));
-				companies = (List<CompanyDto>)serializer.Deserialize(new StringReader(doc.ToString()));
-			}
-		
-		}
+            var response = await _httpClient.GetAsync("companies");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var companies = JsonSerializer.Deserialize<List<CompanyDto>>(content, _options);
+        }
 		public async Task Execute()
 		{
-			await GetCompanies();
-		}
+            //await GetCompanies();
+            await GetCompaniesWithXmlHeader();
+        }
+
+		public async Task GetCompaniesWithXmlHeader()
+		{
+            var request = new HttpRequestMessage(HttpMethod.Get, "companies");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var doc = XDocument.Parse(content);
+            foreach (var element in doc.Descendants())
+            {
+                element.Attributes().Where(a => a.IsNamespaceDeclaration).Remove();
+                element.Name = element.Name.LocalName;
+            }
+            var serializer = new XmlSerializer(typeof(List<CompanyDto>));
+            var companies = (List<CompanyDto>)serializer.Deserialize(new StringReader(doc.ToString()));
+        }
 	}
 }
